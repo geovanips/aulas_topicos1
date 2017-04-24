3d7655faa634a3abd11cc351997e3de8d0e7a542
geovanips <geovanips@outlook.com>
2017-04-24 19:29:13 -0300

    Update Principal.java

--- aulathread/src/aulathread/exemplo/Principal.java
+++ aulathread/src/aulathread/exemplo/Principal.java
@@ -8,6 +8,15 @@ public class Principal {
         
         prog1.start();
         new Thread(prog2).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+               for (int i = 0; i < 100; i++){
+                    System.out.println("Programa " + Thread.currentThread().getId()
+                    + "valor: " + i);
+               } 
+            }
+        }).start();
     }
     
 }
