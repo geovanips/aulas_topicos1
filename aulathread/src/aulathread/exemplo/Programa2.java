cf75277b166fb05f393f4be83da4391a984f906e
geovanips <geovanips@outlook.com>
2017-04-24 19:08:12 -0300

    Update Programa2.java

--- aulathread/src/aulathread/exemplo/Programa2.java
+++ aulathread/src/aulathread/exemplo/Programa2.java
@@ -2,7 +2,7 @@ package aulathread.exemplo;
 
 public class Programa2 implements Runnable{
     public void run(){
-        for (int i = 0; i < 10000; i++){
+        for (int i = 0; i < 100; i++){
             System.out.println("Programa " + Thread.currentThread().getId()
             + "valor: " + i);
         }
