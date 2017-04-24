8dfe3611d29d64b3d02cfa852b1386ee81b15772
geovanips <geovanips@outlook.com>
2017-04-24 19:07:58 -0300

    Update Programa.java

--- aulathread/src/aulathread/exemplo/Programa.java
+++ aulathread/src/aulathread/exemplo/Programa.java
@@ -1,7 +1,7 @@
 package aulathread.exemplo;
 public class Programa extends Thread {
     public void run(){
-        for (int i = 0; i < 10000; i++){
+        for (int i = 0; i < 100; i++){
             System.out.println("Programa " + Thread.currentThread().getId()
             + "valor: " + i);
         }
