58d283891e441d56857e91efcf2cf5ea4d907980
geovanips <geovanips@outlook.com>
2017-04-24 19:04:14 -0300

    Update Principal.java

--- aulathread/src/aulathread/exemplo/Principal.java
+++ aulathread/src/aulathread/exemplo/Principal.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aulathread.exemplo;
 
-/**
- *
- * @author nu3_lab7
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Programa prog1 = new Programa();
+        Programa2 prog2 = new Programa2();
+        
+        prog1.start();
+        new Thread(prog2).start();
     }
     
 }
